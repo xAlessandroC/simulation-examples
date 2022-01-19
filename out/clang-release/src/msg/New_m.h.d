f13e9ef7b47a6e9d074f2514400fb0c93a9c312d
simu5g <simu5g@simu5g-cars-pnp.homenet.telecomitalia.it>
2022-01-19 11:59:32 +0100

    Delete files present in gitignore

--- out/clang-release/src/msg/New_m.h.d
+++
@@ -1,3 +0,0 @@
-msg/New_m.cc msg/New_m.h : \
-	msg/New.msg
-msg/New.msg:
--- out/clang-release/src/nodes/nodeB/NodeB.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/nodes/nodeB/NodeB.o: nodes/nodeB/NodeB.cc \
-  nodes/nodeB/NodeB.h /home/simu5g/omnetpp-6.0pre11/include/omnetpp.h
-
-nodes/nodeB/NodeB.h:
-
-/home/simu5g/omnetpp-6.0pre11/include/omnetpp.h:
